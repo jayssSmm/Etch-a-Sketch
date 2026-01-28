c234ae1b123978f9a3c23b26c272fc45b9262fe2
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-01-28 15:59:52 +0000

    Completed the game, with every element with it's own event listener

--- my_javascript.js
+++ my_javascript.js
@@ -1,23 +1,40 @@
+let body=document.querySelector('body')
+
+const containerSize='640px'
+
 let container=document.querySelector('.container')
 container.style.display='flex'
+container.style.width=containerSize
+container.style.height=containerSize
+container.style.flexWrap='wrap'
+container.style.border='1px solid black'
 
-for (i=1;i<17;i++){
-    let column=document.createElement('div')
-    column.style.flexGrow='1'
-    container.append(column)
+function createDiv(grid){
+    div=document.createElement('div')
+    div.style.border='1px solid black'
+    div.style.boxSizing='border-box'
+    div.style.height=parseInt(containerSize)/grid+'px'
+    div.style.width=parseInt(containerSize)/grid+'px'
+    container.append(div)
 
-    container.append(column)
-    for (j=1;j<17;j++){
-        let div=document.createElement('div')
-        div.setAttribute('id',`${i},${j}`)
-        div.style.border='1px solid black'
-        div.style.padding='5px'
-        div.style.flexGrow='1'
-        column.append(div)
-    }
+    div.addEventListener('mouseenter',(event)=>{
+        let target=event.target
+        target.style.backgroundColor='pink'
+    })
 }
 
-container.addEventListener('mouseover',(event)=>{
-    let target=event.target
-    console.log(target.id)
+for (i=0;i<256;i++){
+    createDiv(16)
+}
+
+btn=document.createElement('button')
+btn.textContent='RESET GRID'
+body.append(btn)
+
+btn.addEventListener('click',()=>{
+    let grid=prompt('Number of squares per side for the new grid: ')
+    container.innerHTML=''
+    for (i=0;i<grid**2;i++){
+        createDiv(grid)
+    }
 })
